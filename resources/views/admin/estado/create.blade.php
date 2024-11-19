3e051727b45973c77ba370ee30cb7e910fca2761
Fernando Jose Uc Poot <fernandopoot247@gmail.com>
2024-11-19 10:56:33 -0600

    Agregando formulario de creacion

--- resources/views/admin/estado/create.blade.php
+++ resources/views/admin/estado/create.blade.php
@@ -1,9 +1,38 @@
 <x-admin-layout>
+    <div class="card bg-white shadow-md rounded-lg overflow-hidden">
 
-    <form action="{{route('admin.estado.store')}}" method="POST">
-        
+        <!-- Card Header -->
+        <div class="card-header bg-blue-200 p-2 flex justify-center items-center">
+            <x-label class="mb-2">
+                CREAR ESTADO
+            </x-label>
+        </div>
 
-        @csrf
-    </form>
+        <form action="{{route('admin.estado.store')}}" method="POST"
+            class="bg-white rounded-lg p-6 shadow-lg">
+
+            <div class="mb-4">
+
+                <x-label class="mb-3">
+                    Nombre:
+                </x-label>
+
+                <x-input
+                name="estado"
+                class="w-full" 
+                placeholder="Escriba el nombre del estado"
+                />
+
+            </div>
+
+            <div class="flex justify-end">
+                <x-button-blue>
+                    Guardar
+                </x-button-blue>
+            </div>
+
+            @csrf
+        </form>
+    </div>
 
 </x-admin-layout>
