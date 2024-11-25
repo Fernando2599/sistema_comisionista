6ab5bf7a85120f71b8f82b34b1135a93351dec81
Fernando Jose Uc Poot <fernandopoot247@gmail.com>
2024-11-24 18:04:22 -0600

    Agregando rutas y vistas correspondientes a roles

--- routes/admin.php
+++ routes/admin.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\Admin\EstadoController;
+use App\Http\Controllers\Admin\RoleController;
 
 Route::get('/', function(){
     return view('admin.dashboard');
 })->name('dashboard');
 
 Route::resource('/estado',EstadoController::class);
+
+Route::resource('roles', RoleController::class);
